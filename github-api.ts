ab59ed1168bcbfc454ba11ecdf93b3acb8a81e80
Jakub Olejník <olejnjak@gmail.com>
2025-02-09 22:06:16 +0100

    Fetch all GitHub releases

--- github-api.ts
+++ github-api.ts
@@ -1,18 +1,57 @@
-import { Octokit } from "@octokit/rest";
+import { Octokit, RestEndpointMethodTypes } from "@octokit/rest";
+import { release } from "os";
 
 const octokit = new Octokit();
 
+interface ReleaseData {
+  tag_name: string;
+  assets: {
+    name: string;
+    download_url: string;
+  }[];
+}
+
+async function fetchAllReleases(owner: string, repo: string): Promise<ReleaseData[]> {
+  var allReleases: ReleaseData[] = [];
+  let page = 1;
+
+  while (true) {
+    const response = await octokit.rest.repos.listReleases({
+      owner,
+      repo,
+      per_page: 100,
+      page,
+    });
+
+    if (response.data.length === 0) break;
+
+    const releases = response.data.map(release => ({
+      tag_name: release.tag_name,
+      assets: release.assets.map(asset => ({
+        name: asset.name,
+        download_url: asset.browser_download_url,
+      })),
+    }));
+
+    allReleases.push(...releases);
+
+    page++;
+  }
+
+  return allReleases;
+}
+
 export async function getReleaseAssets(owner: string, repo: string): Promise<{ [key: string]: string }> {
   try {
-    const response = await octokit.rest.repos.listReleases({ owner, repo });
+    const response = await fetchAllReleases(owner, repo);
     const releaseAssetsMap: { [key: string]: string } = {};
 
-    response.data.forEach(release => {
+    response.forEach(release => {
       const releaseTag = release.tag_name.replace(/[^0-9.]/g, "");
       const xcframeworkAsset = release.assets.find(asset => asset.name.endsWith(".xcframework.zip"));
       
       if (xcframeworkAsset) {
-        releaseAssetsMap[releaseTag] = xcframeworkAsset.browser_download_url;
+        releaseAssetsMap[releaseTag] = xcframeworkAsset.download_url;
       }
     });
 
